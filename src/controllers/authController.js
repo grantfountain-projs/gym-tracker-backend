e08fc529551c4ccb8aa75f358d9f887dfd137b96
gafounta_ncstate <gafounta@ncsu.edu>
2026-01-23 06:08:02 -0500

    Add authentication endpoints (register and login)

---
+++ src/controllers/authController.js
@@ -0,0 +1,67 @@
+import pool from '../config/db.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const saltRounds = 10
+
+const register = async (req, res) => {
+    const { email, password } = req.body; 
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    
+    try {
+        const userCheck = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (userCheck.rows.length > 0) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+
+        const hash = await bcrypt.hash(password, saltRounds);
+        const insertUser = await pool.query('INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *', [email, hash]);
+        return res.status(201).json({ message: "Successfully Registered!" })
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const findUserByEmail = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (findUserByEmail.rows.length == 0) {
+            return res.status(401).json({ message: "User doesn't exist" });
+        }
+        
+        const user = findUserByEmail.rows[0];
+        const hash = user.password_hash;
+        const isMatch = await bcrypt.compare(password, hash);
+        
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid Password" });
+        }
+
+        const payload = {
+            userId: user.id,
+            email: user.email
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        return res.status(200).json({ message: "Login Successful", token: token});
+
+        
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export { register, login };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import pool from './config/db.js'
+import authRouter from './routes/auth.js'
 
 dotenv.config()
 
 
 const app = express()
+app.use(express.json());
+
+app.use('/auth', authRouter);
+
 const port = process.env.PORT || 3000;
 
 // Health Check
