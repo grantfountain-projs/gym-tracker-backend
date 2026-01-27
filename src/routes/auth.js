c0a4cfbf1e3dd5051e4e067f3cb8639efd6f4a63
gafounta_ncstate <gafounta@ncsu.edu>
2026-01-26 19:15:37 -0500

    Added auth middleware and /auth/me endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { register, login } from '../controllers/authController.js'
+import { register, login, me} from '../controllers/authController.js'
+import authMiddleware from '../middleware/auth.js';
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ router.post('/register', register);
 
 router.post('/login', login);
 
-
+router.get('/me', authMiddleware, me);
 
 
 
