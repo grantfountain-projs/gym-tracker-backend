bb3b6cca148c98544d404c78d8020952a829c7e4
gafounta_ncstate <gafounta@ncsu.edu>
2026-02-10 15:01:16 -0500

    Added cors import so frontend can connect to backend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import pool from './config/db.js'
 import authRouter from './routes/auth.js'
 import workoutRouter from './routes/workouts.js'
@@ -9,6 +10,7 @@ dotenv.config()
 
 
 const app = express()
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
